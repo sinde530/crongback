f59a902ca55651e8b8fd0c0c7ad8a32f37004074
kimsungeun <mirxx@naver.com>
2021-07-15 17:17:34 +0900

    modifiy

---
+++ js/component.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent('bbox', {
+    schema: {
+        color: {default: 'pink'},
+        size: {type: 'int', default:5}
+    },
+
+    init: function () {
+    },
+
+    update: function () {
+    },
+
+    remove: function () {
+    },
+
+    tick: function (time, timeDelta) {
+    }
+});
